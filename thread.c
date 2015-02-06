eebcd35ec4e513417cdbf8efb7268a6b8a140eb5
Ursula Sarracini <ursulasarracini@gmail.com>
2015-02-05 20:25:54 -0500

    multi-threading oh ya

---
+++ thread.c
@@ -0,0 +1,96 @@
+/**
+ ** Sarracini
+ ** Ursula
+ ** Section Z
+ ** 211535432
+ ** CSE13208
+ **/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <pthread.h>
+
+typedef struct {
+	char word[101];
+	int frequency;
+} WordArray ;
+
+WordArray words[10000];
+
+int compareWords(const void *f1, const void *f2){
+	WordArray *a = (WordArray *)f1;
+	WordArray *b = (WordArray *)f2;
+	return (b->frequency - a->frequency);
+}
+
+void *countFrequency(void *arg){
+	
+	char *fileName = (char*)arg;
+	int counter = 0;
+	int isUnique;
+	int i;
+	FILE *file;
+	char buff[1000];
+	
+	file = fopen(fileName, "r");
+	if (file == NULL){
+		printf("Couldn't open file: ");
+	}
+	
+	else {
+		while ( (fscanf(file, "%s", buff)) != EOF)
+		{
+			isUnique = -1;
+
+			// String comparison - to check if the word is already in the array
+			int k;
+			for (k = 0; k < counter; k++){
+				if (strcmp(words[k].word, buff) == 0){
+					isUnique = k;
+				}
+			}
+			// If the string is not in the array, put it in the array
+			if (isUnique == -1){
+				strcpy(words[counter].word, buff);
+				words[counter].frequency = 1;
+				counter++;
+			}
+			// Increase frequency of non-unique words
+			else {
+				words[isUnique].frequency++;
+			}	
+		}
+	}	
+	// Sort the words to find most frequent words
+	qsort(words, counter, sizeof(WordArray), compareWords);
+
+	// Store the 3 more frequent words as the result, as a single string
+	char *result = (char*)malloc(sizeof(result));
+	snprintf(result, 100000, "%s %d %s %s %s", fileName, counter, words[0].word, words[1].word, words[2].word);
+	fclose(file);
+	return NULL;
+}
+
+int main(int argc, char *argv[]){
+	
+	pthread_t threads[argc-1];
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+
+	int i;
+	for (i = 1; i <= argc-1; i++){
+		pthread_create(&threads[i], &attr, countFrequency, argv[i]);
+		printf("I created a thread\n");
+	}
+	for (i = 1; i <= argc-1; i++){
+		pthread_join(threads[i], NULL);
+		printf("I'm done joining threads\n");
+	}
+		
+	return 0;
+}
+
